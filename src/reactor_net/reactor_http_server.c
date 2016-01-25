ef815fe961b6523ed7c529afd066029823ed1548
Fredrik Widlund <fredrik.widlund@gmail.com>
2016-01-25 10:37:07 +0000

    Refactor http server reponses

--- src/reactor_net/reactor_http_server.c
+++ src/reactor_net/reactor_http_server.c
@@ -6,7 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <time.h>
 #include <sys/socket.h>
+#include <netinet/tcp.h>
 
 #include <dynamic.h>
 #include <reactor_core.h>
@@ -15,15 +17,79 @@
 #include "reactor_tcp_server.h"
 #include "reactor_http_server.h"
 
+static const char *reactor_http_server_response_code[] =
+  {
+    [100] = "Continue",
+    [101] = "Switching Protocols",
+    [200] = "OK",
+    [201] = "Created",
+    [202] = "Accepted",
+    [203] = "Non-Authoritative Information",
+    [204] = "No Content",
+    [205] = "Reset Content",
+    [206] = "Partial Content",
+    [300] = "Multiple Choices",
+    [301] = "Moved Permanently",
+    [302] = "Found",
+    [303] = "See Other",
+    [305] = "Use Proxy",
+    [306] = "(Unused)",
+    [307] = "Temporary Redirect",
+    [400] = "Bad Request",
+    [401] = "Unauthorized",
+    [402] = "Payment Required",
+    [403] = "Forbidden",
+    [404] = "Not Found",
+    [405] = "Method Not Allowed",
+    [406] = "Not Acceptable",
+    [407] = "Proxy Authentication Required",
+    [408] = "Request Timeout",
+    [409] = "Conflict",
+    [410] = "Gone",
+    [411] = "Length Required",
+    [412] = "Precondition Failed",
+    [413] = "Request Entity Too Large",
+    [414] = "Request-URI Too Long",
+    [415] = "Unsupported Media Type",
+    [416] = "Requested Range Not Satisfiable",
+    [417] = "Expectation Failed",
+    [500] = "Internal Server Error",
+    [501] = "Not Implemented",
+    [502] = "Bad Gateway",
+    [503] = "Service Unavailable",
+    [504] = "Gateway Timeout",
+    [505] = "HTTP Version Not Supported"
+  };
+
 void reactor_http_server_init(reactor_http_server *http_server, reactor_user_call *call, void *state)
 {
   reactor_user_init(&http_server->user, call, state);
   reactor_tcp_server_init(&http_server->tcp_server, reactor_http_server_event, http_server);
+  reactor_timer_init(&http_server->date_update, reactor_http_server_date_event, http_server);
+  reactor_http_server_date_update(http_server);
 }
 
-int reactor_http_server_open(reactor_http_server *http_server, char *node, char *service)
+int reactor_http_server_open(reactor_http_server *http_server, char *node, char *service,
+                             reactor_http_server_options *options)
 {
-  return reactor_tcp_server_open(&http_server->tcp_server, node ? node : "0.0.0.0", service ? service : "http");
+  int e;
+
+  if (options && options->name)
+    {
+      http_server->name = strdup(options->name);
+      if (!http_server->name)
+        return -1;
+    }
+
+  e = reactor_timer_open(&http_server->date_update, 1000000000, 1000000000);
+  if (e == -1)
+    return -1;
+
+  e = reactor_tcp_server_open(&http_server->tcp_server, node ? node : "0.0.0.0", service ? service : "http");
+  if (e == -1)
+    return -1;
+
+  return reactor_tcp_server_set_defer_accept(&http_server->tcp_server, 1);
 }
 
 void reactor_http_server_event(void *state, int type, void *data)
@@ -54,7 +120,9 @@ void reactor_http_server_event(void *state, int type, void *data)
           free(session);
           break;
         }
+      (void) reactor_stream_set_nodelay(&session->stream);
       reactor_user_dispatch(&http_server->user, REACTOR_HTTP_SERVER_ACCEPT, session);
+      reactor_stream_read(&session->stream);
       break;
     case REACTOR_TCP_SERVER_CLOSE:
       reactor_user_dispatch(&http_server->user, REACTOR_HTTP_SERVER_CLOSE, NULL);
@@ -74,6 +142,68 @@ void reactor_http_server_error(reactor_http_server *http_server)
 void reactor_http_server_close(reactor_http_server *http_server)
 {
   reactor_tcp_server_close(&http_server->tcp_server);
+}
+
+void reactor_http_server_respond(reactor_http_server_session *session, unsigned code, char *type,
+                                 char *body, size_t size)
+{
+  reactor_stream *stream;
+  char *message;
+
+  message = NULL;
+  if (code < sizeof reactor_http_server_response_code / sizeof reactor_http_server_response_code[0])
+    message = (char *) reactor_http_server_response_code[code];
+  if (!message)
+    {
+      reactor_http_server_respond(session, 500, NULL, NULL, 0);
+      return;
+    }
+
+  stream = &session->stream;
+  reactor_stream_puts(stream, "HTTP/1.1 ");
+  reactor_stream_putu(stream, code);
+  reactor_stream_puts(stream, " ");
+  reactor_stream_puts(stream, message);
+  reactor_stream_puts(stream, "\r\n");
+  if (session->server->name)
+    {
+      reactor_stream_puts(stream, "Server: ");
+      reactor_stream_puts(stream, session->server->name);
+      reactor_stream_puts(stream, "\r\n");
+    }
+  reactor_stream_puts(stream, "Date: ");
+  reactor_stream_puts(stream, session->server->date);
+  reactor_stream_puts(stream, "\r\nContent-Length: ");
+  reactor_stream_putu(stream, size);
+  if (size)
+    {
+      reactor_stream_puts(stream, "\r\nContent-type: ");
+      reactor_stream_puts(stream, type);
+    }
+  reactor_stream_puts(stream, "\r\n\r\n");
+  if (size)
+    reactor_stream_write(stream, body, size);
+}
+
+void reactor_http_server_date_event(void *state, int type, void *data)
+{
+  (void) data;
+  if (type == REACTOR_TIMER_TIMEOUT)
+    reactor_http_server_date_update(state);
+}
+
+void reactor_http_server_date_update(reactor_http_server *http_server)
+{
+  static const char *days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  static const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  time_t t;
+  struct tm tm;
+
+  (void) time(&t);
+  (void) gmtime_r(&t, &tm);
+  (void) strftime(http_server->date, sizeof http_server->date, "---, %d --- %Y %H:%M:%S GMT", &tm);
+  memcpy(http_server->date, days[tm.tm_wday], 3);
+  memcpy(http_server->date + 8, months[tm.tm_mon], 3);
 }
 
 void reactor_http_server_session_event(void *state, int type, void *data)
@@ -100,7 +230,7 @@ void reactor_http_server_session_data(reactor_http_server_session *session, reac
 {
   reactor_http_server_request *request;
   struct phr_header headers[REACTOR_HTTP_HEADER_MAX_FIELDS];
-  size_t headers_count = REACTOR_HTTP_HEADER_MAX_FIELDS;
+  size_t headers_count;
   size_t method_size, path_size;
   ssize_t body_size;
   int header_size, i;
@@ -115,6 +245,7 @@ void reactor_http_server_session_data(reactor_http_server_session *session, reac
 
       if (!request->base)
         {
+          headers_count = REACTOR_HTTP_HEADER_MAX_FIELDS;
           header_size = phr_parse_request(data->base, data->size, (const char **) &request->method, &method_size,
                                           (const char **) &request->path, &path_size, &request->minor_version,
                                           headers, &headers_count, 0);
@@ -167,24 +298,4 @@ void reactor_http_server_session_data(reactor_http_server_session *session, reac
     }
 
   reactor_stream_flush(&session->stream);
-}
-
-void reactor_http_server_respond(reactor_http_server_session *session, char *code, char *message, char *type,
-				 char *body, size_t size)
-{
-  reactor_stream *stream;
-
-  stream = &session->stream;
-
-  reactor_stream_puts(stream, "HTTP/1.1 ");
-  reactor_stream_puts(stream, code);
-  reactor_stream_puts(stream, " ");
-  reactor_stream_puts(stream, message);
-  reactor_stream_puts(stream, "\r\nContent-type: ");
-  reactor_stream_puts(stream, type);
-  reactor_stream_puts(stream, "\r\nContent-Length: ");
-  reactor_stream_putu(stream, size);
-  reactor_stream_puts(stream, "\r\n\r\n");
-
-  reactor_stream_write(stream, body, size);
 }
